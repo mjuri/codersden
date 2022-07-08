eb21d146bc0a5882e99eef5e102b629fa429e4d3
mvelasco <mariano.juri@gmail.com>
2022-07-08 10:51:57 +0100

    Account WIP

---
+++ profiles/src/main/java/uk/codersden/profiles/Account.java
@@ -0,0 +1,71 @@
+package uk.codersden.profiles;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity(name="accounts")
+public class Account {
+	@Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+	private String identifier;
+	private String name;
+	private String address;
+	private String postcode;
+	private String country;
+	private String telephone;
+	
+    @OneToMany(mappedBy="identifier")
+    private Set<Profile> profiles;
+    
+	public Account() {
+		
+	}
+	public String getIdentifier() {
+		return identifier;
+	}
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+}
