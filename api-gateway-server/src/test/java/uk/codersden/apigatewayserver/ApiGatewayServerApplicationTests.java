70bd6da1af76f191f318d5c8bc83a3fc13918e4f
mvelasco <mariano.juri@gmail.com>
2023-04-11 15:51:42 +0100

    api-gateway-server added

---
+++ api-gateway-server/src/test/java/uk/codersden/apigatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.codersden.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
