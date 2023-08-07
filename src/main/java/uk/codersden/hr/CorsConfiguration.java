627a7bdd8886d5a9ee6c9f307c5ff5b2a5909d94
Mariano Juri Velasco <mariano.juri@gmail.com>
2023-08-07 12:40:07 +0100

    CorsConfiguration added

---
+++ src/main/java/uk/codersden/hr/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package uk.codersden.hr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS for all endpoints
+                .allowedOrigins("*") // Allow requests from any origin (you can specify specific origins if needed)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
+    }
+}
+
