0f83296524ca8152ee620d25503f849ba3553334
Mariano Juri Velasco <mariano.juri@gmail.com>
2023-08-07 13:43:44 +0100

    application.properties changed

--- src/main/java/uk/codersden/hr/CorsConfiguration.java
+++ src/main/java/uk/codersden/hr/CorsConfiguration.java
@@ -11,11 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-        		.allowedOriginPatterns("*") 
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
+        registry.addMapping("/**"); // Allow CORS for all endpoints
     }
 }
 
