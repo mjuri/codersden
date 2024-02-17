d870841bfc5f18148fa0430cfde18e603ef6addd
Mariano Juri Velasco <mariano.juri@gmail.com>
2024-02-17 16:17:10 +0000

    release/0.002 - testing email

--- src/main/java/uk/codersden/hr/mail/EmailSender.java
+++ src/main/java/uk/codersden/hr/mail/EmailSender.java
@@ -22,21 +22,21 @@ public class EmailSender {
 	@GetMapping("/")
 	public ResponseEntity<?> sendTest(){
 
-        final String username = "info@codersden.com";
-        final String password = "7YmBpuu7MH";
-    	//final String username ="mariano.juri@gmail.com";
-    	//final String password = "H4ngth3DJ";
+        //final String username = "info@codersden.com";
+        //final String password = "7YmBpuu7MH";
+    	final String username ="mariano.juri@gmail.com";
+    	final String password = "H4ngth3DJ";
         System.out.println("Starting.............");
         // Set mail properties
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.titan.email");
-        props.put("mail.smtp.port", "465");
         /*props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.titan.email");
+        props.put("mail.smtp.port", "465");*/
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");*/
+        props.put("mail.smtp.port", "587");
         // Create a session with authentication
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
