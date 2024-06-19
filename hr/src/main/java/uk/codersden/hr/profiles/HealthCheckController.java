1f424c6afec693dfdc2dad478bac887c22c6ca6d
Mariano Juri Velasco <mariano.juri@gmail.com>
2024-06-19 14:40:22 +0100

    HealthCheckController.java - WIP

---
+++ hr/src/main/java/uk/codersden/hr/profiles/HealthCheckController.java
@@ -0,0 +1,16 @@
+package uk.codersden.hr.profiles;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+
+	@GetMapping
+	public ResponseEntity<String> healthCheck() {
+		return ResponseEntity.ok("OK");
+	}
+}
